bbaabce06836af347a3d8f4cde8406b8b79f09ef
PawelKlaczynski <125731508+PawelKlaczynski@users.noreply.github.com>
2023-04-02 13:43:52 +0200

    WIP

---
+++ SingletonDemo/Program.cs
@@ -0,0 +1,9 @@
+﻿using SingletonDemo;
+
+Singleton object1 = Singleton.GetInstance;
+object1.Write("Singeltone from object 1");
+
+Singleton object2 = Singleton.GetInstance;
+object1.Write("Singeltone from object 2");
+
+
