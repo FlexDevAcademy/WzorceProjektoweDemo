41e4785059a48d1a0c1724130a84f5a9d2760632
lucosmo <lukasz.kosmaczewski@gmail.com>
2023-04-02 09:00:53 +0100

    Created classes Band and Restaurant

---
+++ FasadaDemo/BrideFacade.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FasadaDemo
+{
+    public class BrideFacade
+    {
+        public double WeddingCosts = 0;
+        public string MakeWedding()
+        {
+
+        }
+    }
+}
---
+++ FasadaDemo/Restaurant.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FasadaDemo
+{
+    public class Restaurant
+    {
+        private int Ingredients = 0;
+
+        public double EstimateCosts(int guest)
+        {
+            return guest * 180;
+        }
+
+        public void BuyIngredients(int guest)
+        {
+            Ingredients = guest;
+        }
+
+        public bool GetDishes(int guest)
+        {
+            if (Ingredients <= guest)
+            {
+                return true;
+            }
+            return false;
+        }
+    }
+}
