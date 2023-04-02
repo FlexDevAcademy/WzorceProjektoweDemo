34bde22b6e9f360f2f52fe6e8feb814679a168b1
daria956 <125731362+daria956@users.noreply.github.com>
2023-04-02 15:49:32 +0200

    Create Builder design patterns

--- BuilderDemo/Program.cs
+++ BuilderDemo/Program.cs
@@ -4,26 +4,26 @@
     {
         static void Main(string[] args)
         {
-            Director director = new Director();
+Director director = new Director();
             HouseBuilder houseBuilder = new HouseBuilder();
             director.Builder = houseBuilder;
-            
-            Console.WriteLine("Build basic house");
-            director.BuildBasicHouse();
+
+Console.WriteLine("Build basic house");
+director.BuildBasicHouse();
             var house = houseBuilder.GetHouse();
             foreach(var part in house.parts)
-            {
-                Console.WriteLine(part);
-            }
+{
+    Console.WriteLine(part);
+}
 
             Console.WriteLine("Build luxury house");
-            director.BuildLuxuryHouse();
+director.BuildLuxuryHouse();
             var houseWithPool = houseBuilder.GetHouse();
 
-            foreach (var part in houseWithPool.parts)
-            {
-                Console.WriteLine(part);
-            }
+foreach (var part in houseWithPool.parts)
+{
+    Console.WriteLine(part);
+}
 
             Console.ReadLine();
         }
