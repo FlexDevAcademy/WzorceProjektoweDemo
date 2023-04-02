4cbeb399d5d1b54a9048db4a88203a746e3695c2
PawelKlaczynski <125731508+PawelKlaczynski@users.noreply.github.com>
2023-04-02 10:11:06 +0200

    wip

--- FasadaDemo/BrideFasade.cs
+++ FasadaDemo/BrideFasade.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -8,9 +9,10 @@ namespace FasadaDemo
 {
     public class BrideFasade
     {
+
         public double WeddingCosts = 0;
 
-        public string MakeWeeding(int guests, int members)
+        public string MakeWedding()
         {
             Restaurant restaurant = new Restaurant();
             Band band = new Band();
@@ -19,7 +21,7 @@ namespace FasadaDemo
             WeddingCosts += restaurant.EstimateCosts(guests);
             WeddingCosts += band.EstimateCosts(members);
             WeddingCosts += flovershop.EstimateCosts();
-
+            
             restaurant.BuyIngredients(guests);
             flovershop.MakeFlovers();
             band.MakeAConcert();
