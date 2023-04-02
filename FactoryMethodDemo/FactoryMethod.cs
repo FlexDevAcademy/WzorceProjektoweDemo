f660c4746803464f34ad8a4583e85b6df5c97e5d
daria956 <125731362+daria956@users.noreply.github.com>
2023-04-02 14:33:57 +0200

    Create FactoryMethod

---
+++ FactoryMethodDemo/FactoryMethod.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FactoryMethodDemo
+{
+   abstract class Transport
+    {
+        public abstract IPackage FactoryMethod();
+
+        public string LogisticOperation()
+        {
+            Console.WriteLine("Creating instance of FactoryMethod");
+            var package = FactoryMethod();
+
+            Console.WriteLine("Delivering package started");
+            var result = package.DeliverPackage();
+
+            return "Package delivered: " + result;
+        }
+    }
+    class Package1 : IPackage
+    {
+        public string DeliverPackage()
+        {
+            return "Delivering package 1";
+        }
+
+    }
+    class Package2 : IPackage
+    {
+        public string DeliverPackage()
+        {
+            return "Delivering package 2";
+        }
+
+    }
+    class DHL : Transport
+    {
+        public override IPackage FactoryMethod()
+        {
+            return new Package1();
+        }
+    }
+    class InPost : Transport
+    {
+        public override IPackage FactoryMethod()
+        {
+            return new Package2();
+        }
+ 
+    }
+    public interface IPackage
+    {
+        string DeliverPackage();
+
+    }
+    class Client
+    {
+        public void Main()
+        {
+            Console.WriteLine("Using DHL");
+            UseCompany(new DHL());
+
+            Console.WriteLine("Using InPost");
+            UseCompany(new InPost());
+        }
+
+        public void UseCompany(Transport transport)
+        {
+            Console.WriteLine("Starting to use transport" + transport.GetType().ToString());
+            Console.WriteLine(transport.LogisticOperation());
+        }
+    }
+}
