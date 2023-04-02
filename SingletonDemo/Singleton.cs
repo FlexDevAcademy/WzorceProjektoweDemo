06f175089d9cdf3956ac40623592b05c5ad7bb27
lucosmo <lukasz.kosmaczewski@gmail.com>
2023-04-02 12:18:32 +0100

    Create Singleton class

---
+++ SingletonDemo/Singleton.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SingletonDemo
+{
+    public sealed class Singleton
+    {
+        private static int counter = 0;
+        private static Singleton instance = null;
+        public static Singleton GetInstance
+        {
+            get
+            {
+                if (instance == null)
+                {
+                    instance = new Singleton();
+                }
+                return instance;
+            }
+        }
+
+        private Singleton()
+        {
+            counter++;
+            Console.WriteLine("counter value: " + counter.ToString());
+        }
+
+        public void Write(string text)
+        {
+            Console.WriteLine(text);
+        }
+    }
+}
