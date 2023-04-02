513da14feb65621b3a116dcd3b1e79d4625a889e
PawelKlaczynski <125731508+PawelKlaczynski@users.noreply.github.com>
2023-04-02 11:43:18 +0200

    WIP

---
+++ MediatorDemo/Mediator.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MediatorDemo
+{
+     public interface IMediator
+    {
+        void Notify(object sender, string ev);
+
+    }
+
+    class BaseComponent
+    {
+        protected IMediator _mediator;
+
+        public BaseComponent(IMediator mediator = null)
+        {
+            this._mediator= mediator;
+        }
+
+        public void SetMediator(IMediator mediator)
+        {
+            this._mediator= mediator;
+        }
+    }
+
+    class Component1: BaseComponent
+    {
+        public void DoA()
+        {
+            Console.WriteLine("Component1 does A");
+            this._mediator.Notify(this, "A");
+        }        
+        
+        
+        public void DoB()
+        {
+            Console.WriteLine("Component1 does B");
+            this._mediator.Notify(this, "B");
+        }
+    }    
+    
+    class Component2: BaseComponent
+    {
+        public void DoC()
+        {
+            Console.WriteLine("Component1 does C");
+            this._mediator.Notify(this, "C");
+        }        
+        
+        
+        public void DoD()
+        {
+            Console.WriteLine("Component1 does D");
+            this._mediator.Notify(this, "D");
+        }
+    }
+}
