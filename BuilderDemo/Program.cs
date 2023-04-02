bec7fa07c5021804788f927d94320e8a82c7de7d
PawelKlaczynski <125731508+PawelKlaczynski@users.noreply.github.com>
2023-04-02 15:39:48 +0200

    WIP

---
+++ BuilderDemo/Program.cs
@@ -0,0 +1,33 @@
+﻿namespace BuilderDemo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Director director = new Director();
+            HouseBuilder houseBuilder = new HouseBuilder();
+            director.Builder = houseBuilder;
+            Console.WriteLine("Building basic house");
+            director.BuildBasicHouse();
+            var house = houseBuilder.GetHouse();
+
+            foreach (var part in house.parts)
+            {
+                Console.WriteLine(part);
+            }
+
+            director.BuildLuxuryHouse();
+            var houseWithPool = houseBuilder.GetHouse();
+
+            foreach (var part in houseWithPool.parts)
+            {
+                Console.WriteLine(part);
+            }
+
+
+
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
