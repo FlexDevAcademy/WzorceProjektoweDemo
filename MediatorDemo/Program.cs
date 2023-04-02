ab994405e71c624e9673f732e328c7e4d0043e04
daria956 <125731362+daria956@users.noreply.github.com>
2023-04-02 12:27:28 +0200

    Create a component1 and component 2 instance

--- MediatorDemo/Program.cs
+++ MediatorDemo/Program.cs
@@ -1,2 +1,13 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using MediatorDemo;
+
+//Console.WriteLine("Hello, World!");
+
+Component1 component1= new Component1();
+Component2 component2= new Component2();
+
+new ConcreteMediator(component1, component2);
+Console.WriteLine("Triggers operation A");
+component1.DoA();
+Console.WriteLine("Triggers opeartion B");
+component2.DoD();
