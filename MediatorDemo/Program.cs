9b967e20d36a646af73814284af5a9f3e53686d2
Maci-ctrl <125731455+Maci-ctrl@users.noreply.github.com>
2023-04-02 12:27:39 +0200

    Edit main Program

--- MediatorDemo/Program.cs
+++ MediatorDemo/Program.cs
@@ -1,2 +1,18 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using MediatorDemo;
+
+Component1 component1 = new Component1();
+Component2 component2 = new Component2();
+
+new ConcreteMediator(component1, component2);
+
+Console.WriteLine("Client triggers operation A: ");
+component1.DoA();
+
+Console.WriteLine("Client triggers operation D: ");
+component2.DoD();
+
+
+
+
+
