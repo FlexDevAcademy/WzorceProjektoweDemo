a42386743227f7c1b02a35f7c5b8b858b3411385
daria956 <125731362+daria956@users.noreply.github.com>
2023-04-02 13:42:58 +0200

    Create Singleton

---
+++ SingletonDemo/Program.cs
@@ -0,0 +1,8 @@
+﻿// See https://aka.ms/new-console-template for more information
+using SingletonDemo;
+
+Singleton object1 = Singleton.GetInstance;
+object1.Write("Singleton from object 1");
+
+Singleton object2 = Singleton.GetInstance;
+object2.Write("Singleton from object 2");
