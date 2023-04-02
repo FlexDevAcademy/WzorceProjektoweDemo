ddc685b80d9aeb5b4bbfb73df6fdabd672bb06bf
daria956 <125731362+daria956@users.noreply.github.com>
2023-04-02 15:49:32 +0200

    Create Builder design patterns

---
+++ BuilderDemo/Program.cs
@@ -0,0 +1,20 @@
+﻿// See https://aka.ms/new-console-template for more information
+using BuilderDemo;
+
+Director director = new Director();
+HouseBuilder housebuilder = new HouseBuilder();
+director.Builder= housebuilder;
+
+Console.WriteLine("Build basic house");
+director.BuildBasicHouse();
+var house = housebuilder.GetHouse();
+foreach (var part in house.parts)
+{
+    Console.WriteLine(part);
+}
+director.BuildLuxuryHouse();
+var houseWithPool = housebuilder.GetHouse();
+foreach (var part in houseWithPool.parts)
+{
+    Console.WriteLine(part);
+}
