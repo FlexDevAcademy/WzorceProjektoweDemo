45de06f6a3f41e51b81717fce3d804d8c3726cd8
lucosmo <lukasz.kosmaczewski@gmail.com>
2023-04-02 14:49:07 +0100

    Create Builder pattern

---
+++ BuilderDemo/Builder.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BuilderDemo
+{
+    public interface IBuilder
+    {
+        public void BuildFloors();
+        public void BuildWalls();
+        public void BuildRoof();
+
+        public void BuildSwimmingPool();
+    }
+
+    public class Director
+    {
+        private IBuilder _builder;
+        public IBuilder Builder
+        {
+            set { _builder = value; }
+        }
+
+        public void BuildBasicHouse()
+        {
+            this._builder.BuildFloors();
+            this._builder.BuildWalls();
+            this._builder.BuildRoof();
+        }
+
+        public void BuildLuxuryHouse()
+        {
+            this._builder.BuildFloors();
+            this._builder.BuildWalls();
+            this._builder.BuildRoof();
+            this._builder.BuildSwimmingPool();
+        }
+    }
+
+    public class House
+    {
+        public List<string> parts = new List<string>();
+        public void Add(string part)
+        {
+            this.parts.Add(part);
+        }
+
+    }
+
+    public class HouseBuilder : IBuilder
+    {
+        House house = new House();
+
+        public HouseBuilder()
+        {
+            this.Reset();
+        }
+        public void Reset()
+        {
+            this.house = new House();
+        }
+        public void BuildFloors()
+        {
+            this.house.Add("Add floors");
+        }
+
+        public void BuildRoof()
+        {
+            this.house.Add("Add roof");
+        }
+
+        public void BuildSwimmingPool()
+        {
+            this.house.Add("Add swimming pool");
+        }
+
+        public void BuildWalls()
+        {
+            this.house.Add("Add walls");
+        }
+
+        public House GetHouse() 
+        {
+            House result = this.house;
+            this.Reset();
+            return result;
+        }
+    }
+}
