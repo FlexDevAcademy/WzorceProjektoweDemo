e0c06a108c70cce2ceeb49532fb2afd89eeb17f5
PawelKlaczynski <125731508+PawelKlaczynski@users.noreply.github.com>
2023-04-02 10:11:06 +0200

    wip

---
+++ FasadaDemo/BrideFasade.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FasadaDemo
+{
+    public class BrideFasade
+    {
+
+        public double WeddingCosts = 0;
+
+        public string MakeWedding()
+        {
+            Restaurant restaurant = new Restaurant();
+            
+        }
+    }
+}
