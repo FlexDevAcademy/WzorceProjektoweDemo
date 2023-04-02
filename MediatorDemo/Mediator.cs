cc993c02372ba5e949347c6a1f69cae1b8d94bfa
lucosmo <lukasz.kosmaczewski@gmail.com>
2023-04-02 11:02:23 +0100

    add CopncreteMediator

--- MediatorDemo/Mediator.cs
+++ MediatorDemo/Mediator.cs
@@ -12,6 +12,36 @@ namespace MediatorDemo
 
     }
 
+    class ConcreteMediator : IMediator
+    {
+        private Component1 _component1;
+        private Component2 _component2;
+
+        public ConcreteMediator(Component1 component1, Component2 component2)
+        {
+            this._component1 = component1;
+            this._component2 = component2;
+
+            this._component1.SetMediator(this);
+            this._component2.SetMediator(this);
+        }
+
+        public void Notify(object sender, string operation)
+        {
+            if(operation == "A")
+            {
+                Console.Writeline("Mediator reacts on action A and triggers on following operations: ");
+                this._component2.DoC();
+            }
+            if (operation == "D")
+            {
+                Console.Writeline("Mediator reacts on action D and triggers on following operations: ");
+                this._component1.DoB();
+            }
+
+        }
+    }
+
     class BaseComponent
     {
         protected IMediator _mediator;
