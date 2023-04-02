0fac937ec46a144fe9a0e593704b80473c8804f8
daria956 <125731362+daria956@users.noreply.github.com>
2023-04-02 12:03:16 +0200

    Create Class Component1 and Component2

---
+++ MediatorDemo/Mediator.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MediatorDemo
+{
+   public interface IMediator
+    {
+        void Notify(object sender, string ev);
+    }
+    class ConcreteMediator : IMediator
+    {
+        private Component1 _component1;
+        private Component2 _component2;
+
+        public ConcreteMediator(Component1 component1, Component2 component2)
+        {
+            this._component1 = component1;
+            this._component2= component2;
+            this._component1.SetMediator(this);
+            this._component2.SetMediator(this);
+        }
+        public void Notify(object sender, string operation)
+        {
+            if (operation== "A")
+            {
+                Console.WriteLine("Mediator react on action A and riggers on following operation.");
+                this._component2.DoC();
+            }
+            if (operation == "D")
+            {
+                Console.WriteLine("Mediator react on action D and riggers on following operation.");
+                this._component1.DoB();
+            }
+        }
+    }
+    class BaseComponent
+    {
+        protected IMediator _mediator;
+
+        public BaseComponent(IMediator mediator = null)
+        {
+            this._mediator = mediator;
+        }
+        public void SetMediator(IMediator mediator) 
+        { 
+            this._mediator = mediator;
+        }
+       
+    }
+    class Component1 : BaseComponent
+    {
+        public void DoA()
+        {
+            Console.WriteLine("Component DoA");
+            this._mediator.Notify(this, "A");
+        }
+        public void DoB()
+        {
+            Console.WriteLine("Component DoB");
+            this._mediator.Notify(this, "B");
+        } }
+    class Component2 : BaseComponent { 
+        public void DoC()
+        {
+            Console.WriteLine("Component DoC");
+            this._mediator.Notify(this, "C");
+        }
+        public void DoD()
+        {
+            Console.WriteLine("Component DoD");
+            this._mediator.Notify(this, "D");
+        }
+    }
+}
